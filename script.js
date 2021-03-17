7d2b62e09412e839e47523589550437403c06c10
Alopmel <78558722+Alopmel@users.noreply.github.com>
2021-03-17 18:31:22 +0100

    delete dir My-Backpacking and image, script.js, style.css, Historyculture.html and History

--- script.js
+++
@@ -1,7 +0,0 @@
-const ham = document.querySelector('.ham');
-const links = document.querySelector('.menu-links');
-
-ham.addEventListener('click', () =>{
-    links.classList.toggle('activate');
-});
-
