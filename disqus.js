7f12d81a70ef0d3f565912154ee73763c6384765
Alopmel <78558722+Alopmel@users.noreply.github.com>
2021-03-23 13:44:10 +0100

    footer / contactUs

--- disqus.js
+++
@@ -1,9 +0,0 @@
-
-   /* var disqus_config = function () {*/
-   
-    (function() { // DON'T EDIT BELOW THIS LINE
-    var d = document, s = d.createElement('script');
-    s.src = 'https://backpacking-through-spain.disqus.com/embed.js';
-    s.setAttribute('data-timestamp', +new Date());
-    (d.head || d.body).appendChild(s);
-    })();
